0fba67a305f575e733701a2879f3b757383a2cf0
a-wave-maker <wawron.adam@gmail.com>
2023-10-22 22:13:25 +0200

    add project files

---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D playerRB;
+    private float playerMoveSpeed;
+    private Vector2 playerMoveDirection;
+
+    private Weapon[] playerWeapons;
+    private int playerActiveWeaponIndex;
+    private Weapon playerActiveWeapon;
+
+    public Rigidbody2D PlayerRB { get => playerRB; set => playerRB = value; }
+    public float PlayerMoveSpeed { get => playerMoveSpeed; set => playerMoveSpeed = value; }
+    public Vector2 PlayerMoveDirection { get => playerMoveDirection; set => playerMoveDirection = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
