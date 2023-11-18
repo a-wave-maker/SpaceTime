0d453e2f2821c0d0505631f7375227ccf2ce9fad
a-wave-maker <wawron.adam@gmail.com>
2023-11-18 21:57:30 +0100

    Separated SuperHot logic from GameManager and fixed a small issue with it

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -5,9 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    private PlayerData playerData;
-
     public enum GameState
     {
         MainMenu,
@@ -18,12 +15,6 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance; // Singleton pattern
 
     public GameState currentState = GameState.MainMenu;
-
-    private bool modeSuperHot = false;
-    private float superHotMin = 0;
-    private float superHotMax = 50;
-
-    public bool ModeSuperHot { get => modeSuperHot; set => modeSuperHot = value; }
 
     void Start()
     {
@@ -40,23 +31,7 @@ public class GameManager : MonoBehaviour
 
         // Subscribe to the PlayerDeath event
         Player.PlayerDeath += HandlePlayerDeath;
-        PlayerController.ChangeSuperHot += changeMode;
-    }
 
-    private void Update()
-    {
-        if (modeSuperHot)
-        {
-            setTimeScale(mappedSpeed() + 0.1f);
-        }
-    }
-    private float mappedSpeed()
-    {
-        float clampedSpeed = Mathf.Clamp(playerData.PlayerRB.velocity.magnitude, superHotMin, superHotMax);
-
-        float mappedValue = Mathf.Lerp(0f, 0.9f, clampedSpeed / superHotMax);
-
-        return mappedValue;
     }
 
     public void StartGame()
@@ -76,16 +51,5 @@ public class GameManager : MonoBehaviour
     {
         currentState = GameState.GameOver;
         SceneManager.LoadScene("MainMenu");
-    }
-
-    public void setTimeScale(float amount)
-    {
-        Time.timeScale = amount;
-        Time.fixedDeltaTime = 0.02F * Time.timeScale;
-    }
-
-    public void changeMode()
-    {
-        modeSuperHot = !modeSuperHot;
     }
 }
