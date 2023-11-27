e4cc7352297bf1592012c7e2b8f2c490fbc07ee9
Zuzanna Urbanska <zuz.urbanska@wp.pl>
2023-11-27 16:43:06 +0100

    moved some PlayerWeapon attributes back to Weapon script for convenience

--- Assets/Scripts/Weapons/PlayerWeapons/PlayerWeapon.cs
+++ Assets/Scripts/Weapons/PlayerWeapons/PlayerWeapon.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public class PlayerWeapon : Weapon
 {
 
-    private float fireRate; // bullets per second
-    private float recoil;
-    private float reloadTime; // how long it takes to reload
-    private int maxAmmo;
-
     private int remainingAmmo;
     private float lastFireTime;
     private float reloadCooldown;
@@ -18,45 +13,20 @@ public class PlayerWeapon : Weapon
     private bool isReloading = false;
 
 
-    public float FireRate
-    {
-        get { return fireRate; }
-        set { fireRate = value; }
-    }
-
-    public float Recoil
-    {
-        get { return recoil; }
-        set { recoil = value; }
-    }
-
-    public float ReloadTime
-    {
-        get { return reloadTime; }
-        set { reloadTime = value; }
-    }
-
-    public int MaxAmmo
-    {
-        get { return maxAmmo; }
-        set { maxAmmo = value; }
-    }
-
-
     protected override void Start()
     {
         base.Start();
-        lastFireTime = - (1 / fireRate);
-        remainingAmmo = maxAmmo;
+        lastFireTime = - (1 / FireRate);
+        remainingAmmo = MaxAmmo;
         reloadCooldown = Time.time;
     }
 
     void Update()
     {
         // if reloading -> check if finished
-        if (isReloading && Time.time - reloadCooldown >= reloadTime)
+        if (isReloading && Time.time - reloadCooldown >= ReloadTime)
         {
-            remainingAmmo = maxAmmo;
+            remainingAmmo = MaxAmmo;
             isReloading = false;
         }
     }
@@ -82,8 +52,7 @@ public class PlayerWeapon : Weapon
     private bool CanFire()
     {
         bool hasAmmo = remainingAmmo > 0;
-        print(hasAmmo);
-        float shotCooldown = 1 / fireRate;
+        float shotCooldown = 1 / FireRate;
         return hasAmmo && Time.time - lastFireTime >= shotCooldown;
     }
 
--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -5,14 +6,46 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+
+    private float fireRate; // bullets per second
+    private float recoil;
+    private float reloadTime; // how long it takes to reload
+    private int maxAmmo;
+
     [SerializeField] private Bullet bullet;
     private GameObject owner; // who has the weapon
+
+
+    public float FireRate
+    {
+        get { return fireRate; }
+        set { fireRate = value; }
+    }
+
+    public float Recoil
+    {
+        get { return recoil; }
+        set { recoil = value; }
+    }
+
+    public float ReloadTime
+    {
+        get { return reloadTime; }
+        set { reloadTime = value; }
+    }
+
+    public int MaxAmmo
+    {
+        get { return maxAmmo; }
+        set { maxAmmo = value; }
+    }
 
     public GameObject Owner
     {
         get { return owner; }
         set { owner = value; }
     }
+
 
     protected virtual void Start()
     {
@@ -32,13 +65,7 @@ public class Weapon : MonoBehaviour
     }
 
 
-    public virtual void Reload()
-    {
+    public virtual void Reload(){}
 
-    }
-
-    public virtual void Switch()
-    {
-
-    }
+    public virtual void Switch(){}
 }
