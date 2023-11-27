7f98b47840396450790f0ca5ab8535ae59464103
Zuzanna Urbanska <zuz.urbanska@wp.pl>
2023-11-27 20:39:46 +0100

    bullet trail visuals

--- Assets/Scripts/Weapons/Bullets/Bullet.cs
+++ Assets/Scripts/Weapons/Bullets/Bullet.cs
@@ -1,51 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     private float speed = 10f;
     private Vector3 weaponVelocity = Vector3.zero;
-    private float damage = 1f;
+    private int damage = 5;
 
     private Rigidbody2D rb;
     private GameObject owner;
-    // private TrailRenderer trail;
+    private TrailRenderer trail;
 
     private float lifeTime = 5f;
     // public float trailtime = 0.5f;
+    private float trailWidthDecreaseRate = 0.3f;
+    private float trailLengthDecreaseRate = 0.0005f;
 
-    public float Speed
-    {
-        get { return speed; }
-        set { speed = value; }
-    }
-    public Vector3 WeaponVelocity
-    {
-        get { return weaponVelocity; }
-        set { weaponVelocity = value; }
-    }
 
-    public float Damage
-    {
-        get { return damage; }
-        set { damage = value; }
-    }
-    public GameObject Owner
-    {
-        get { return owner; }
-        set { owner = value; }
-    }
+    public Vector3 WeaponVelocity { get => weaponVelocity; set => weaponVelocity = value; }
+    public GameObject Owner { get => owner; set => owner = value; }
+    public int Damage { get => damage; set => damage = value; }
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(transform.up * speed + weaponVelocity, ForceMode2D.Impulse);
+        rb.AddForce(transform.up * speed + WeaponVelocity, ForceMode2D.Impulse);
 
-        // trail = GetComponentInChildren<TrailRenderer>();
+        trail = GetComponentInChildren<TrailRenderer>();
 
         Invoke("DestroyBullet", lifeTime);
     }
+
+    void Update()
+    {
+        // decrease the trail following bullet
+        if (trail != null)
+        {
+            trail.widthMultiplier -= trailWidthDecreaseRate * Time.deltaTime;
+            trail.time -= trailLengthDecreaseRate;
+
+            if (trail.widthMultiplier < 0.05f)
+            {
+                trail.widthMultiplier = 0.05f;
+            }
+            if (trail.time < 0f)
+            {
+                trail.time = 0f;
+            }
+        }
+    }
+
 
     void DestroyBullet()
     {
