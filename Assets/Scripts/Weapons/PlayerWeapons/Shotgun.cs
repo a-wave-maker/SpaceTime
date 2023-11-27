ae800582a7aef27cc7fc969fe6ce8cfa44e93b9e
Zuzanna Urbanska <zuz.urbanska@wp.pl>
2023-11-27 20:08:46 +0100

    shotgun shoots multiple bullets

--- Assets/Scripts/Weapons/PlayerWeapons/Shotgun.cs
+++ Assets/Scripts/Weapons/PlayerWeapons/Shotgun.cs
@@ -6,19 +6,25 @@ public class Shotgun : PlayerWeapon
 {
     protected override void Start()
     {
-        FireRate = 8f;
-        Recoil = 16f;
-        ReloadTime = 1f;
-        MaxAmmo = 10;
+        FireRate = 1f;
+        Recoil = 15f;
+        ReloadTime = 0.3f;
+        MaxAmmo = 1;
         WeaponName = "Shotgun";
         // test values ^
         base.Start();
     }
 
-    // void Update()
-    // {
+    private void SpawnBullet(Quaternion direction)
+    {
+        Bullet newBullet = Instantiate(Bullet, transform.position, direction);
 
-    // }
+        if (Owner != null)
+        {
+            newBullet.WeaponVelocity = Owner.GetComponent<Rigidbody2D>().velocity;
+            newBullet.Owner = Owner;
+        }
+    }
 
     public override bool Fire(Quaternion? direction = null)
     {
@@ -28,11 +34,13 @@ public class Shotgun : PlayerWeapon
         }
         if (CanFire())
         {
-            Bullet newBullet = Instantiate(Bullet, transform.position, (Quaternion)direction);
+            float spreadAngle = 25f;
 
-            if (Owner != null) {
-                newBullet.WeaponVelocity = Owner.GetComponent<Rigidbody2D>().velocity;
-                newBullet.Owner = Owner;
+            for (int i = 0; i < 7; i++)
+            {
+                Quaternion spreadDirection = (Quaternion)direction * Quaternion.Euler(0, 0, Random.Range(-spreadAngle, spreadAngle));
+
+                SpawnBullet(spreadDirection);
             }
             LastFireTime = Time.time;
             RemainingAmmo--;
