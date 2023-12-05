36b68937da2bd8d17c6b8d1ee3b72245eccc7beb
a-wave-maker <wawron.adam@gmail.com>
2023-12-05 18:33:51 +0100

    Added some setup for UI

---
+++ Assets/Scripts/UI/Game/Crosshair.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // hide cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cursorScreenPos = Input.mousePosition;
+
+        transform.position = cursorScreenPos;
+    }
+}
