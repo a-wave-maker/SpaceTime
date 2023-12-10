98d1156715e126c24807de48ed76ce7e4c5cae68
a-wave-maker <wawron.adam@gmail.com>
2023-12-10 16:17:13 +0100

    Changed the UI to be camera bound

--- Assets/Scripts/Game/CameraFollow.cs
+++ Assets/Scripts/Game/CameraFollow.cs
@@ -4,19 +4,56 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
+    [SerializeField] private Transform target;
 
-    public Vector3 offset = new Vector3(0f, 0f, -10f);
+    [SerializeField] private Vector3 offset = new Vector3(0f, 0f, -10f);
+    
+    [SerializeField] private float baseSize = 20f;
+    [SerializeField] private float maxSize = 40f;
+    [SerializeField] private float panSpeed = 5f;
+    [SerializeField] private float panSize = 20f;
 
-    void LateUpdate()
+    private void LateUpdate()
     {
         if (target != null)
         {
-            // Set the camera's position to the target's position plus the offset
+            // follow the target
             transform.position = target.position + offset;
-        } else
-        {
 
+            // set the zoom
+
+            // set the offset
+        }
+        else
+        {
+            Debug.Log("Camera has no target");
         }
     }
+
+    private void setSize()
+    {
+
+    }
+
+    private void setPan()
+    {
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
+
+        float panX = 0f;
+        float panY = 0f;
+
+        if (mouseX < 10f)
+            panX = -1f;
+        else if (mouseX > Screen.width - 10f)
+            panX = 1f;
+
+        if (mouseY < 10f)
+            panY = -1f;
+        else if (mouseY > Screen.height - 10f)
+            panY = 1f;
+
+
+    }
 }
+
