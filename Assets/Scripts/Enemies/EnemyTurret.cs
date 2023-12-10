22e5c3424da680a64fe2b3c70da4340fa68e8fa9
Michal <michalgwrobel@gmail.com>
2023-12-10 17:59:10 +0100

    refactor

--- Assets/Scripts/Enemies/EnemyTurret.cs
+++ Assets/Scripts/Enemies/EnemyTurret.cs
@@ -7,14 +7,14 @@ public class EnemyTurret : MonoBehaviour
     Rigidbody2D rigidbody;
 
     [SerializeField] public Transform target;
-    [SerializeField] public GameObject Bullet;
+    [SerializeField] public GameObject bullet = null;
     [SerializeField] public bool moveable = false;
     [SerializeField] public double mass = double.PositiveInfinity;
     [SerializeField] public float fireRate = 2f;
     [SerializeField] public float Damage = 69f;
     [SerializeField] public float bulletForce = 69f;
     [SerializeField] public float projectileSpeed = 0.1f;
-    [SerializeField] public float rotateSpeed = 60f;
+    [SerializeField] public float rotationSpeed = 60f;
 
     [SerializeField] public float viewRadius = 0f;
     [Range(0,360)]
@@ -22,37 +22,45 @@ public class EnemyTurret : MonoBehaviour
 
 
 
-    public EnemyState currentState = EnemyState.Idle;
+    public EnemyState currentState = EnemyState.Attack;
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        target = GameObject.FindGameObjectWithTag("player").GetComponent<Transform>();
-        currentState = EnemyState.Idle;
-        Vector2 direction = (target.position - transform.position).normalized;  
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     void Update()
     {
         switch(currentState)
         {
+            case EnemyState.Attack:
+                Attack();
+                break;
             case EnemyState.Idle:
                 Patrol();
-                break;
-            case EnemyState.Attack:
                 break;
         }
     }
     
     void Attack(){
-
+        LookAtTarget();
     }
 
     void Patrol(){
-        transform.Rotate(rotateSpeed * Time.deltaTime * Vector3.forward);
+        transform.Rotate(rotationSpeed * Time.deltaTime * Vector3.forward);
         
     }
 
+    void LookAtTarget()
+    {
+        Vector3 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle + 90f));
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+    }
 }
 
 
